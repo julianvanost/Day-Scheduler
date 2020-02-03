3f15521c90a511bf978a4e56e26377f49fd4aef2
Julian <Van Ost>
2020-02-02 19:28:36 -0800

    adjustments to styling and logic

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -1,14 +1,28 @@
 $(document).ready(function (event) {
-  let hours = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm']
-  let plan = JSON.parse(localStorage.getItem('toDoListTable')) || []
-  plan.length = hours.length
+  let hoursArr = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm']
+  let toDo = JSON.parse(localStorage.getItem('toDoListTable')) || []
+  toDo.length = hoursArr.length
+  let currentTime = moment()
+  $('#currentDay').html(currentTime.format('MMMM DD, YYYY'))
 
-  let now = moment()
-  // let todayHeader = now.format('LL')
-  // $("#currentDay").html(todayHeader)
-
+  const createHourlyRows = _ => {
+    for (let i = 0; i < hoursArr.length; i++) {
+      $('#toDoListTable').append(
+        `<div id=box${i} class='text-lg-left z-depth-4 ${colorRow(i)}'>` +
+        `<div class="modal-header"><label class="textarea2" for=input${i}>${hoursArr[i]}` +
+        `</label><textarea id=input${i} class="description materialize-textarea" placeholder="Add To-Do Item"></textarea>` +
+        `<div class="col-md-1"><button id=save${i} class="saveBtn btn-floating btn-large waves-effect waves-light blue">Add` +
+        `</button>`
+      )
+    }
+    for (let i = 0; i < hoursArr.length; i++) {
+      if (toDo[i]) {
+        document.getElementById(`input${i}`).value = toDo[i]
+      }
+    }
+  }
   const colorRow = x => {
-    const hour = parseInt(parseInt(now.format('H')) - 9)
+    const hour = parseInt(parseInt(currentTime.format('H')) - 9)
     if (x < hour) {
       return 'past'
     } else if (x > hour) {
@@ -16,45 +30,23 @@ $(document).ready(function (event) {
     }
     return 'current'
   }
-
-  const buildTable = _ => {
-    $('#currentDay').textContent = now.format('dddd, MMMM D, YYYY')
-    for (let i = 0; i < hours.length; i++) {
-      $('#toDoListTable').append(
-        `<div id=box${i} class='${colorRow(i)}' text-lg-left z-depth-4">` +
-        `<div class="modal-header"><label class="textarea2" for=input${i}>${hours[i]}` +
-        `</label><input id=input${i} class="description materialize-textarea" placeholder="Add To-Do Item"></input>` +
-        `<div class="col-md-1"><button id=save${i} class="saveBtn btn-floating btn-large waves-effect waves-light blue">Add` +
-        `</button>`
-      )
-    }
-    for (let i = 0; i < hours.length; i++) {
-      if (plan[i]) {
-        // console.log(`input${i}`)
-        document.getElementById(`input${i}`).value = plan[i]
-      }
-    }
+  const saveArrToLocalStorage = id => {
+    const keyValPair = id[4]
+    toDo[keyValPair] = document.getElementById(`input${keyValPair}`).value
+    localStorage.setItem('toDoListTable', JSON.stringify(toDo))
   }
+  const saveArray = { save1: saveArrToLocalStorage, save2: saveArrToLocalStorage, save3: saveArrToLocalStorage, save4: saveArrToLocalStorage, save5: saveArrToLocalStorage, save6: saveArrToLocalStorage, save7: saveArrToLocalStorage, save8: saveArrToLocalStorage, save9: saveArrToLocalStorage, save0: saveArrToLocalStorage }
 
-  const doSave = id => {
-    const slot = id[4]
-    plan[slot] = document.getElementById(`input${slot}`).value
-    localStorage.setItem('toDoListTable', JSON.stringify(plan))
-  }
-
-  const saveArray = { save1: doSave, save2: doSave, save3: doSave, save4: doSave, save5: doSave, save6: doSave, save7: doSave, save8: doSave, save9: doSave, save0: doSave }
-
-  const setListener = _ => {
+  const saveTargetValues = _ => {
     $('#toDoListTable').on('click', event => {
-      // console.log(event.target.id)
       try {
         saveArray[event.target.id](event.target.id)
       } catch (e) {
-        console.log('No action taken!')
+
       }
     })
   }
 
-  setListener()
-  buildTable()
+  saveTargetValues()
+  createHourlyRows()
 })
